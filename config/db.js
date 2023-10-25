3f328691cb5abfdf06686bcbfa8b8f3495777b13
Ubuntu <ubuntu@ip-172-31-35-160.ec2.internal>
2023-10-25 10:24:38 +0000

    hi

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://52.91.163.65:27017/BlogApp").then(()=>{
-    console.log("connected!");
-}).catch((err)=>{
-    console.log(err);
-    console.log("error");
-})
-console.log("hell");
